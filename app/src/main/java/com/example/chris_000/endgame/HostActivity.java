a6ed48592be5d0b2cac494028edd7bf74e845bfc
Christoffer Maarup Noerbjerg <christoffernoerbjerg@me.com>
2014-11-12 12:50:29 +0100

    Stoppet med fragments, activities er oprettet istedet

--- app/src/main/java/com/example/chris_000/endgame/HostActivity.java
+++ app/src/main/java/com/example/chris_000/endgame/HostActivity.java
@@ -3,23 +3,25 @@ package com.example.chris_000.endgame;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class InitActivity extends Activity {
 
+public class HostActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.init_activity);
+        setContentView(R.layout.host_activity);
 
-        final Button hostButton = (Button) findViewById(R.id.host_button);
+        final Button hostButton = (Button) findViewById(R.id.host_back_button);
         hostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent changeActivity = new Intent(InitActivity.this,HostActivity.class);
-                InitActivity.this.startActivity(changeActivity);
+                Intent changeActivity = new Intent(HostActivity.this,InitActivity.class);
+                HostActivity.this.startActivity(changeActivity);
             }
         });
     }
