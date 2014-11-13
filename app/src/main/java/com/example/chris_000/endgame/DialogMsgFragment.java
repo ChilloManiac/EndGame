329d4a817fcd8391b7f36e83c0e1fba640cbf316
André Akselsen <afakselsen@gmail.com>
2014-11-13 14:31:29 +0100

    Updated dialogMsgFragment

--- app/src/main/java/com/example/chris_000/endgame/DialogMsgFragment.java
+++ app/src/main/java/com/example/chris_000/endgame/DialogMsgFragment.java
@@ -1,5 +1,6 @@
 package com.example.chris_000.endgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,8 +17,15 @@ public class DialogMsgFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //buttonMsg.setOnClickListener();
+        buttonMsg.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View popupView) {
+                Intent changeActivity = new Intent(getActivity(), GameActivity.class);
+                startActivity(changeActivity);
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_dialog_msg, container, false);
+
     }
 }
